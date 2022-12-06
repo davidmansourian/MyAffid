42c4358cf8762543023885dc6a93c5f300b8b340
davidmansourian <mada20sv@student.ju.se>
2022-12-06 10:24:54 +0100

    Pushing structures worked on in separate file

---
+++ Affid/ViewModels/BarChartViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  BarChartViewModel.swift
+//  BreathingCircle
+//
+//  Created by David on 2022-12-05.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class BarChartViewModel: ObservableObject{
+    private var coreDataManager = CoreDataManager.shared
+    @Published var yearlyChart: [Bar] = []
+    private var monthlyCountArr: [MonthCount] = []
+    
+    
+    init(){
+        loadBarChart()
+    }
+    
+    
+    func loadMonthCountWithMonth(){
+        for i in 1...12{
+            monthlyCountArr.append(MonthCount(month: i, count: 0))
+        }
+    }
+    
+    
+    func getYearlyStats(){
+        for session in coreDataManager.getSessionsPerMonth(){
+            let monthInt = Calendar.current.component(.month, from: session.date!)
+            print(monthInt)
+            monthlyCountArr[monthInt-1].count += 1 // minus 1 because dictionary loads from 0 to 11
+        }
+        print(monthlyCountArr)
+    }
+    
+    func loadBarChart(){
+        monthlyCountArr.removeAll()
+        loadMonthCountWithMonth()
+        getYearlyStats()
+        let color: Color = ColorData.shared.appSystemYellow
+        let months = ["January", "February", "Mmars", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        
+        for month in monthlyCountArr{
+            let barItem = Bar(name: "\(month.month)", month: months[month.month-1], value: Double(month.count), color: color)
+            print (month.count)
+            yearlyChart.append(barItem)
+        }
+        
+        print(yearlyChart)
+        
+    }
+}
---
+++ Affid/ViewModels/QuotesViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  QuotesViewModel.swift
+//  BreathingCircle
+//
+//  Created by David on 2022-12-05.
+//
+
+import Foundation
+import Combine
+
+
+class QuotesViewModel: ObservableObject{
+    private var apiService = APIService()
+    private var coreDataManager = CoreDataManager.shared
+    @Published var randomQuote: QuotesModel?
+    @Published var totalCompletedMeditations: Int?
+    private var cancellables = Set<AnyCancellable>()
+    
+    func getQuote(){
+        apiService.$quotesArr
+            .sink { [weak self] data in
+                self?.randomQuote = data.randomElement()
+            }.store(in: &cancellables)
+    }
+    
+    func getTotalMeditationsCompleted(){
+        totalCompletedMeditations = coreDataManager.countTotalTimesMeditated()
+    }
+    
+}
